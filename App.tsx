13915a666b80d48c9f43535261ae0c99c9505692
João Gabriel <jgabriel.ferreira20@gmail.com>
2024-11-22 03:35:18 -0300

    feat(p2-pdm): adiciona botao e exibe fotos em pilha

--- App.tsx
+++ App.tsx
@@ -1,25 +1,29 @@
-import React, { useEffect, useState } from 'react';
-import { StyleSheet, View, Text, Image, FlatList,} from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Text, Image, ScrollView, Button} from 'react-native';
 import axios from 'axios';
 
 
 export default function App() {
   const [fotos, setFotos] = useState([]);
 
-  useEffect(() => {
-    axios.get('https://api.thecatapi.com/v1/images/search?limit=5')
-      .then(response => setFotos(response.data))
-      .catch(error => console.error(error));
-  }, []);
+  const fetchFotos= async () => {
+    try {
+      const res = await axios.get('https://api.thecatapi.com/v1/images/search?limit=5');
+      setFotos([...fotos, ...res.data]);
+    } catch (error) {
+      console.error('Erro ao obter as fotos:', error);
+    }
+  };
 
   return (
-    <FlatList
-      data={fotos}
-      keyExtractor={(item) => item.id}
-      renderItem={({ item }) => (
-        <Image source={{ uri: item.url }} style={styles.image} />
-      )}
-    />
+    <View style={styles.container}>
+      <Button title="Carregar Fotos" onPress={fetchFotos} />
+      <ScrollView style={styles.scrollView}>
+        {fotos.map((photo, index) => (
+          <Image key={index} source={{ uri: photo.url }} style={styles.image} />
+        ))}
+      </ScrollView>
+    </View>
   );
 };
 
@@ -30,9 +34,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-    image: { 
-      alignItems: 'center',
-      width: 300, 
-      height: 300, 
-      margin: 10 }
+  image: { 
+    width: 300, 
+    height: 300, 
+    margin: 10 
+  },
+  ScrollView: {
+    marginTop: 5,
+  }
 });
